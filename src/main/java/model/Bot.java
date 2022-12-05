17d2d663bc95b0b237ac0ac62832a86ca79344e7
belmella <sami.belmellat@etu.u-paris.fr>
2022-12-05 22:48:36 +0100

    <Functions> Added <Class Bot> Finie Bot créé Reste : - Tests - Implemetation

--- src/main/java/model/Bot.java
+++ src/main/java/model/Bot.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.util.Random;
+
 public class Bot implements RacketController {
     private State state ;
     private int difficulte ; 
     private boolean renvoieBalle ; 
-    private boolean calculDirection ; 
+    private boolean calculRenvoieBalle ; 
 
     Bot (int difficulte) {
         this.difficulte = difficulte ; 
@@ -16,11 +18,71 @@ public class Bot implements RacketController {
         return state ; 
     }
 
-    public void calculDirection (double posX , double posY , double nextBallX , double nextBallY) {
-        // calcule la direction vers laquelle la raquette doit se diriger
+    private int calculDirection (double posY , double nextBallY) {
+        if (nextBallY > posY) return -1 ; // la balle se dirige vers le bas
+        if (nextBallY < posY) return 1 ; // la balle se dirige vers le haut
+        return 0 ; // la balle reste immobile
     }
 
-    public void play () {
-        // fonctioin qui va faire jouer le bot
+    private void moveRandomDirection (Random rm) {
+        int direction = rm.nextInt(100) ; 
+            if (direction < 10) {
+                state = State.IDLE ; 
+                return ; 
+            }
+            if (direction >= 10 && direction < 50 ) {
+                state = State.GOING_UP ; 
+                return ; 
+            }
+            if (direction >= 50) {
+                state = State.GOING_DOWN ; 
+                return ; 
+            }
+    }
+
+    public void play (double posX , double nextBallX , double width) {
+        Random rm = new Random() ; 
+        if (posX <= width/2) {// on bouge aleatoirement
+            renvoieBalle = false ; 
+            calculRenvoieBalle = false ; 
+            moveRandomDirection(rm);
+            return ; 
+        }
+        
+        if (renvoieBalle) {// on doit renvoyer la balle
+            switch(calculDirection(posX , nextBallX)) {
+                case -1 :
+                    state = State.GOING_DOWN ; 
+                    break ; 
+                case 0 :
+                    state = State.IDLE ; 
+                    break ;
+                case 1 :
+                    state = State.GOING_UP ; 
+                    break ;         
+            }
+            return ; 
+        }
+
+        if (!calculRenvoieBalle) {// on doit savoir si on renvoit la balle ou pas
+            calculRenvoieBalle = true ; 
+            switch(difficulte) {
+                case 1 :
+                    renvoieBalle = rm.nextInt(4) == 1 ;
+                    // renvoie 1/4 des balles
+                    break ; 
+                case 2 :
+                    renvoieBalle = rm.nextInt(5) >= 2 ; 
+                    // renvoie 3/5 des balles
+                    break ; 
+                case 3 :
+                    renvoieBalle = rm.nextInt(100) >= 10 ; 
+                    // renvoie 90/100 balles
+                    break ;          
+            }
+            return ; 
+        }
+    
+        moveRandomDirection(rm);
     }
 }
