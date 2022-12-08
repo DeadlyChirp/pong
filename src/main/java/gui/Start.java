cb04a61be7f4c9b7fdcf853ac6ea60bf58b58d90
driche <ismael.driche@etu.u-paris.fr>
2022-12-08 12:01:53 +0100

    Correction du bug du timer qui ne se ferme avec la fenêtre issue #42

--- src/main/java/gui/Start.java
+++ src/main/java/gui/Start.java
@@ -2,7 +2,9 @@ package gui;
 import javafx.scene.layout.Pane;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.stage.*;
+import javafx.event.EventHandler;
+import model.TimeMode;
 
 /*********************************************************************************************************************** */
 
@@ -15,6 +17,11 @@ public class Start extends Application {
         Scene gameScene = new Scene(root) ;
         Menu a = new Menu(root, gameScene);
         a.start(primaryStage);
+        primaryStage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent window) {
+                TimeMode.closeTimer();
+            }
+        });
     }
 
 }
